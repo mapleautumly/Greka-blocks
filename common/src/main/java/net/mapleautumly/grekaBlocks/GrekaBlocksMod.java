a2304d870a534b6e8c7c0de6e63e44408106266b
mapleautumly <maautumple@gmail.com>
2026-04-07 14:44:12 -0600

    Add files via upload

---
+++ common/src/main/java/net/mapleautumly/grekaBlocks/GrekaBlocksMod.java
@@ -0,0 +1,24 @@
+package net.mapleautumly.grekaBlocks;
+
+import net.mapleautumly.grekaBlocks.blocks.GrecaBlocks;
+import net.mapleautumly.grekaBlocks.items.GrecaItems;
+import net.mapleautumly.grekaBlocks.tab.GrecaTabs;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class GrekaBlocksMod {
+    public static final String MOD_ID = "greka-blocks";
+
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    public static void init() {
+        // Write common init code here.
+        LOGGER.info("Initializing Greca Blocks Mod");
+        GrecaItems.register();
+        GrecaBlocks.register();
+        GrecaTabs.register();
+
+    }
+
+}
